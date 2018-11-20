9bfb7d7268b364833833d35d548184607b7637ef
AdnrewRoni <andrewroni@gmail.com>
2018-11-20 12:19:51 +0200

    Add Store method to controller. Add Validation

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -3,8 +3,8 @@
 // Bring in model
 const Post = use('App/Models/Post')
 
-
-
+// Bring in Validator
+const { validate } = use('Validator')
 
 class PostController {
   async index({ view }) {
@@ -32,6 +32,30 @@ class PostController {
 
   async add({ view }) {
     return view.render('posts.add')
+  }
+
+  async store({ request, response, session }) {
+    // Validaton input
+    const validation = await validate(request.all(), {
+      title: 'required|min:3|max:255',
+      body: 'required|min:3'
+    })
+
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+    const post = new Post();
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Post Added!' })
+
+    return response.redirect('/posts')
   }
 }
 
